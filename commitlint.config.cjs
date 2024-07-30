b9526a181927b5dca130828e753b5f46e61fa2fa
NZMia <mia7zwy@hotmail.com>
2024-07-30 22:27:55 +1200

    workflow: modified the commitlint file to match CI

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -2,26 +2,14 @@
 module.exports = {
   extends: [],
   rules: {
-    'header-end-period': [2, 'always'],
-    'type-enum': [2, 'always', ['Feat', 'Bug', 'Workflow', 'Style', 'Chore']],
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'bug', 'workflow', 'style', 'chore', 'test'],
+    ],
     'type-empty': [2, 'never'],
     'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
     'header-min-length': [2, 'always', 10],
-    'header-case-start-capital': [2, 'always'],
   },
-  plugins: [
-    {
-      rules: {
-        'header-case-start-capital': ({ raw }) => {
-          return [
-            /^[A-Z]/.test(raw),
-            'Commit message must start with a capital letter',
-          ];
-        },
-        'header-end-period': ({ header }) => {
-          return [/\.$/.test(header), 'Commit message must end with a period'];
-        },
-      },
-    },
-  ],
 };
