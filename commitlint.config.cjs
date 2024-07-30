04c9d0087c0095e705e8ed8fd2c000c4f10ede65
NZMia <mia7zwy@hotmail.com>
2024-07-30 21:19:11 +1200

    Workflow: testing.

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,29 +1,25 @@
+// ref: https://github.com/conventional-changelog/commitlint/tree/master/%40commitlint/config-conventional
 module.exports = {
-  // extends: ['@commitlint/config-conventional'],
   extends: [],
   rules: {
     'header-end-period': [2, 'always'],
     'type-enum': [2, 'always', ['Feat', 'Bug', 'Workflow', 'Style', 'Chore']],
-    'type-case': [2, 'always', 'lower-case'],
     'type-empty': [2, 'never'],
     'subject-empty': [2, 'never'],
-    'subject-case': [2, 'always', 'lower-case'],
-    'header-pattern': [2, 'always', /^(\w+):\s.+$/],
     'header-min-length': [2, 'always', 10],
+    'header-case-start-capital': [2, 'always'],
   },
   plugins: [
     {
       rules: {
+        'header-case-start-capital': ({ raw }) => {
+          return [
+            /^[A-Z]/.test(raw),
+            'Commit message must start with a capital letter',
+          ];
+        },
         'header-end-period': ({ header }) => {
           return [/\.$/.test(header), 'Commit message must end with a period'];
-        },
-        'type-enum': ({ raw }) => {
-          const types = ['Feat', 'Bug', 'Workflow', 'Style', 'Chore'];
-          const type = raw.split(':')[0];
-          return [
-            types.includes(type),
-            `Commit message must start with one of: ${types.join(', ')}`,
-          ];
         },
       },
     },
